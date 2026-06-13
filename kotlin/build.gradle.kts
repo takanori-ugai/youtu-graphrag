8c3060ed80e79ad9ff96231103f9a57b9bd4ce7f
Takanori Ugai <ugai.takanori@gmail.com>
2026-06-13 14:07:29 +0900

    Update build.gradle.kts

--- kotlin/build.gradle.kts
+++ kotlin/build.gradle.kts
@@ -73,7 +73,7 @@ dependencies {
     implementation("dev.langchain4j:langchain4j-open-ai:1.15.0")
     implementation("dev.langchain4j:langchain4j-open-ai-official:1.16.1-beta26")
     implementation("dev.langchain4j:langchain4j-ollama:1.15.0")
-    implementation("dev.langchain4j:langchain4j-community-neo4j:1.16.1-beta26")
+    implementation("dev.langchain4j:langchain4j-community-neo4j:1.16.0-beta26")
 
     // Koin for Ktor
     implementation("io.insert-koin:koin-ktor:$koinVersion")
